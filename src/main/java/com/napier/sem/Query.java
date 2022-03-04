1632a311f6d75e36782b57513332bb71d9d5b8f9
Declan Fisher <declan.fisher01@outlook.com>
2022-03-04 15:39:57 +0000

    Query: Population of continents

--- src/main/java/com/napier/sem/Query.java
+++ src/main/java/com/napier/sem/Query.java
@@ -319,9 +319,10 @@ public class Query {
                 Statement stmt = connection.createStatement();
                 // Create string for SQL statement
                 String strSelect =
-                        "SELECT continent, population " +
+                        "SELECT continent, COUNT(*) AS 'population' p " +
                                 "FROM country c " +
-                                "ORDER BY population DESC";
+                                "GROUP BY continent " +
+                                "ORDER BY p DESC";
                 // Execute SQL statement
                 ResultSet rset = stmt.executeQuery(strSelect);
                 // Return query result if query is successful
