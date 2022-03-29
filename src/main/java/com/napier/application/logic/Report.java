e9f4bbe4507a5025977bc8aca51e47d65c0a2a47
Declan Fisher <declan.fisher01@outlook.com>
2022-03-29 16:31:10 +0100

    Added final queries/reports

--- src/main/java/com/napier/application/logic/Report.java
+++ src/main/java/com/napier/application/logic/Report.java
@@ -147,4 +147,149 @@ public class Report {
             return null;
         }
     }
+
+    public ArrayList<Country> populationReport(String option) {
+
+        if (option == null) {
+            throw new IllegalArgumentException("Option for this query must be specified");
+        }
+
+        try {
+            if (option.equals("Continent")) {
+                // Create an SQL statement
+                Statement stmt = connection.createStatement();
+                // Create string for SQL statement
+                String strSelect =
+                        "SELECT co.Continent AS Continent, SUM(co.Population) AS Total_Pop, " +
+                                "SUM(ci.Population) AS In_city, " +
+                                "round(((SUM(ci.Population)/SUM(co.Population))*100),2) AS In_City_Perc, " +
+                                "SUM(co.Population) - SUM(ci.Population) AS Out_city, " +
+                                "round((((SUM(co.Population) - SUM(ci.Population))/SUM(co.Population))*100),2) AS Out_City_Perc " +
+                                "FROM country co " +
+                                "JOIN (SELECT " +
+                                "ci.countrycode " +
+                                ",SUM(ci.population) AS Population " +
+                                "FROM city ci GROUP BY 1) ci ON ci.countrycode = co.code " +
+                                "GROUP BY Continent";
+                // Execute SQL statement
+                ResultSet rset = stmt.executeQuery(strSelect);
+                // Return query result if query is successful
+                // Check one is returned
+                ArrayList<Country> countries = new ArrayList<Country>();
+                while (rset.next()) {
+                    Country country = new Country();
+                    country.Continent = rset.getString("Continent");
+                    country.Population = rset.getLong("Total_Pop");
+                    country.InCityPop = rset.getLong("In_city");
+                    country.InCityPerc = rset.getLong("In_City_Perc");
+                    country.OutCityPop = rset.getLong("Out_city");
+                    country.OutCityPerc = rset.getLong("Out_City_Perc");
+                    countries.add(country);
+                }
+                System.out.println("Population Report By Continent");
+                System.out.println(String.format("%-10s %25s %25s %25s %25s %25s ",
+                        "Continent", "Total Population", "Population In Cities", "Population In Cities (%)", "Population Out of Cities", "Population Out of Cities (%)"));
+                // Loop over all employees in the list
+                for (Country country : countries) {
+                    String string = String.format("%-10s %25s %25s %25s %25s %25s ",
+                            country.Continent, country.Population, country.InCityPop, country.InCityPerc, country.OutCityPop, country.OutCityPerc);
+                    System.out.println(string);
+                }
+                return countries;
+
+            } else if (option.equals("Region")) {
+                // Create an SQL statement
+                Statement stmt = connection.createStatement();
+                // Create string for SQL statement
+                String strSelect =
+                        "SELECT co.Region AS Region, SUM(co.Population) AS Total_Pop, " +
+                                "SUM(ci.Population) AS In_city, " +
+                                "round(((SUM(ci.Population)/SUM(co.Population))*100),2) AS In_City_Perc, " +
+                                "SUM(co.Population) - SUM(ci.Population) AS Out_city, " +
+                                "round((((SUM(co.Population) - SUM(ci.Population))/SUM(co.Population))*100),2) AS Out_City_Perc " +
+                                "FROM country co " +
+                                "JOIN (SELECT " +
+                                "ci.countrycode " +
+                                ",SUM(ci.population) AS Population " +
+                                "FROM city ci GROUP BY 1) ci ON ci.countrycode = co.code " +
+                                "GROUP BY Region";
+                // Execute SQL statement
+                ResultSet rset = stmt.executeQuery(strSelect);
+                // Return query result if query is successful
+                // Check one is returned
+                ArrayList<Country> countries = new ArrayList<Country>();
+                while (rset.next()) {
+                    Country country = new Country();
+                    country.Region = rset.getString("Region");
+                    country.Population = rset.getLong("Total_Pop");
+                    country.InCityPop = rset.getLong("In_city");
+                    country.InCityPerc = rset.getLong("In_City_Perc");
+                    country.OutCityPop = rset.getLong("Out_city");
+                    country.OutCityPerc = rset.getLong("Out_City_Perc");
+                    countries.add(country);
+                }
+                System.out.println("Population Report By Region");
+                System.out.println(String.format("%-10s %25s %25s %25s %25s %25s ",
+                        "Region", "Total Population", "Population In Cities", "Population In Cities (%)", "Population Out of Cities", "Population Out of Cities (%)"));
+                // Loop over all employees in the list
+                for (Country country : countries) {
+                    String string = String.format("%-10s %25s %25s %25s %25s %25s ",
+                            country.Region, country.Population, country.InCityPop, country.InCityPerc, country.OutCityPop, country.OutCityPerc);
+                    System.out.println(string);
+                }
+                return countries;
+
+            } else if (option.equals("Country")) {
+                // Create an SQL statement
+                Statement stmt = connection.createStatement();
+                // Create string for SQL statement
+                String strSelect =
+                        "SELECT co.Name AS Name, SUM(co.Population) AS Total_Pop, " +
+                                "SUM(ci.Population) AS In_city, " +
+                                "round(((SUM(ci.Population)/SUM(co.Population))*100),2) AS In_City_Perc, " +
+                                "SUM(co.Population) - SUM(ci.Population) AS Out_city, " +
+                                "round((((SUM(co.Population) - SUM(ci.Population))/SUM(co.Population))*100),2) AS Out_City_Perc " +
+                                "FROM country co " +
+                                "JOIN (SELECT ci.countrycode, SUM(ci.population) AS Population " +
+                                "FROM city ci GROUP BY 1) ci ON ci.countrycode = co.code " +
+                                "GROUP BY Name";
+                // Execute SQL statement
+                ResultSet rset = stmt.executeQuery(strSelect);
+                // Return query result if query is successful
+                // Check one is returned
+                ArrayList<Country> countries = new ArrayList<Country>();
+                while (rset.next()) {
+                    Country country = new Country();
+                    country.Name = rset.getString("Name");
+                    country.Population = rset.getLong("Total_Pop");
+                    country.InCityPop = rset.getLong("In_city");
+                    country.InCityPerc = rset.getLong("In_City_Perc");
+                    country.OutCityPop = rset.getLong("Out_city");
+                    country.OutCityPerc = rset.getLong("Out_City_Perc");
+                    countries.add(country);
+                }
+                System.out.println("Population Report By Country");
+                System.out.println(String.format("%-10s %25s %25s %25s %25s %25s ",
+                        "Country", "Total Population", "Population In Cities", "Population In Cities (%)", "Population Out of Cities", "Population Out of Cities (%)"));
+                // Loop over all employees in the list
+                for (Country country : countries) {
+                    String string = String.format("%-10s %25s %25s %25s %25s %25s ",
+                            country.Name, country.Population, country.InCityPop, country.InCityPerc, country.OutCityPop, country.OutCityPerc);
+                    System.out.println(string);
+                }
+                return countries;
+
+            } else {
+                throw new IllegalArgumentException("Invalid option specified");
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to execute populationInAndOutOfCities");
+            return null;
+        }
+    }
 }
