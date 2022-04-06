16080a1095f0758249d39090f9e8fe6872908321
haskett88 <40543989@live.napier.ac.uk>
2022-04-06 13:36:47 +0100

    Added SUM and GROUP BY statements

--- src/main/java/com/napier/sem/Query.java
+++ src/main/java/com/napier/sem/Query.java
@@ -1346,7 +1346,7 @@ public class Query {
                     ///        + "WHERE A.Language IN('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic') "
                     ///        + "GROUP BY A.Language "
                     ///        + "ORDER BY 2 DESC;";
-            "SELECT A.Language, SUM((A.Percentage/100) * (B.Population/1000000)) AS TotalSpeakers, (SELECT SUM(Population) from country) AS PercentOfWorldPop "
+            "SELECT A.Language, SUM((A.Percentage/100) * (B.Population/1000000)) AS TotalSpeakers, (SELECT SUM(Population/1000000) from country) AS PercentOfWorldPop "
                     + "FROM countrylanguage A "
                     + "LEFT JOIN country B ON A.CountryCode = B.Code "
                     + "WHERE A.Language IN('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic') "
