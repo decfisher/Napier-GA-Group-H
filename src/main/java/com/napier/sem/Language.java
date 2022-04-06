6773509d17e726e9d03a7ca0ee7109401e3ef550
haskett88 <40543989@live.napier.ac.uk>
2022-04-06 11:22:04 +0100

    Test of leftjoin between countrylanguage and country

--- src/main/java/com/napier/sem/Language.java
+++ src/main/java/com/napier/sem/Language.java
@@ -1,6 +1,7 @@
 package com.napier.sem;
 
 public class Language {
+    public String Code;
     public String Language;
     public double TotalSpeakers;
     public double PercentOfWorldPop;
--- src/main/java/com/napier/sem/Query.java
+++ src/main/java/com/napier/sem/Query.java
@@ -1346,8 +1346,9 @@ public class Query {
                     ///        + "WHERE A.Language IN('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic') "
                     ///        + "GROUP BY A.Language "
                     ///        + "ORDER BY 2 DESC;";
-            "SELECT A.Language, A.Percentage AS TotalSpeakers, A.Percentage/100 AS PercentOfWorldPop "
+            "SELECT A.Language, B.Code, A.Percentage AS TotalSpeakers, b.Population AS PercentOfWorldPop "
                     + "FROM countrylanguage A "
+                    + "LEFT JOIN country B ON A.CountryCode = B.Code "
                     + "WHERE A.Language IN('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic');";
 
             // Execute SQL statement
