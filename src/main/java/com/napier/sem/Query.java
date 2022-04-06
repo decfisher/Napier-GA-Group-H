90ac3d92372080e9b7fe5bfafb587eeb34a4d12d
haskett88 <40543989@live.napier.ac.uk>
2022-04-06 13:54:18 +0100

    Added ORDER BY clause and rounding to calculated fields

--- src/main/java/com/napier/sem/Query.java
+++ src/main/java/com/napier/sem/Query.java
@@ -1346,7 +1346,7 @@ public class Query {
                     ///        + "WHERE A.Language IN('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic') "
                     ///        + "GROUP BY A.Language "
                     ///        + "ORDER BY 2 DESC;";
-            "SELECT A.Language, ROUND(SUM((A.Percentage/100) * (B.Population/1000000)),2) AS TotalSpeakers, ROUND(SUM((A.Percentage/100) * (B.Population/1000000))/(SELECT SUM(Population/1000000) from country),2) AS PercentOfWorldPop "
+            "SELECT A.Language, ROUND(SUM((A.Percentage/100) * (B.Population/1000000)),2) AS TotalSpeakers, ROUND((SUM((A.Percentage/100) * (B.Population/1000000))/(SELECT SUM(Population/1000000) from country))*100,2) AS PercentOfWorldPop "
                     + "FROM countrylanguage A "
                     + "LEFT JOIN country B ON A.CountryCode = B.Code "
                     + "WHERE A.Language IN('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic') "
