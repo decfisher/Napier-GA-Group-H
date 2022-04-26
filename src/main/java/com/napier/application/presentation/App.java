a5e16722d8279ec996f2714b2de99eb5a2494efc
Declan Fisher <declan.fisher01@outlook.com>
2022-04-26 17:27:50 +0100

    Updated report outputs to not output null values

--- src/main/java/com/napier/application/presentation/App.java
+++ src/main/java/com/napier/application/presentation/App.java
@@ -21,16 +21,16 @@ public class App {
         } else {
             a.connect(args[0], Integer.parseInt(args[1]));
         }
-        // Create query object to initialise queries
-//        Query query = new Query(a.getConnection());
-//        query.getLanguagePopularity();
-        Report report = new Report(a.getConnection());
-        report.countryReport();
-        report.cityReport();
-        report.capitalCityReport();
-        report.populationReport("Continent");
-        report.populationReport("Region");
-        report.populationReport("Country");
+//         Create query object to initialise queries
+        Query query = new Query(a.getConnection());
+        query.getPopulationOf();
+//        Report report = new Report(a.getConnection());
+//        report.countryReport();
+//        report.cityReport();
+//        report.capitalCityReport();
+//        report.populationReport("Continent");
+//        report.populationReport("Region");
+//        report.populationReport("Country");
 
         a.disconnect(); // Disconnect from database
     }
